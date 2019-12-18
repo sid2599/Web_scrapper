5b0b32fa036187924de7a5adfdcdb28b591056f9
Siddharth <siddharthjp1999@gmail.com>
2019-12-18 11:45:33 +0530

    clean code

--- page_traversal.py
+++ page_traversal.py
@@ -13,58 +13,50 @@ db = client.reviewtest
 
 #Accesing drivers of firefox
 driver = webdriver.Firefox()
-#driver.get('https://www.tripadvisor.in/Restaurants-g32655-Los_Angeles_California.html')
+
 
 driver.get('https://www.tripadvisor.in/Hotels-g60763-oa180-New_York_City_New_York-Hotels.html')
 print(driver.title)
+
+#Storing the main window handle
 main_window = driver.current_window_handle
 
-#Restaurant hyperlinks
-# restuarnt res_buttons = driver.find_elements_by_class_name('restaurants-list-ListCell__restaurantName--2aSdo')
-
+# Getting the hyperlinks of hotel names
 res_buttons = driver.find_elements_by_class_name('property_title.prominent')
 
-#res_buttons = res_buttons[2:len(res_buttons)]
-#print(len(res_buttons)) 
+ 
 
-
+#Iterating through each hotel hyperlink
 for i in range(len(res_buttons)):
  #clicking the resaturant hyperlink
  if res_buttons[i].is_displayed():
   driver.execute_script("arguments[0].click();",res_buttons[i])
   time.sleep(3)
 
-  #switching tab to new restaurant
+  #Setting tab to new hotel
   driver.switch_to.window(driver.window_handles[1])
 
-  #Expanding hidden reviews
-  #more_buttons = driver.find_elements_by_class_name('taLnk.ulBlueLinks')
+  #Expanding hidden reviews by clicking the (read more) button
   more_buttons = driver.find_elements_by_class_name("hotels-hotel-review-about-csr-Description__readMore--3rZ3t")
-  #print(len(more_buttons))
+  
   if len(more_buttons)!=0:
    if more_buttons[0].is_displayed():
       driver.execute_script("arguments[0].click();", more_buttons[0])
       time.sleep(4)
 
-  #time.sleep(3)
+  
   print(' ')
   print('Hotel name:',driver.title)
   print(' ')
-  #test for review scraping
-  
-  #a = driver.find_elements_by_class_name('partial_entry')
-  #DOV = driver.find_element_by_class_name("prw_rup.prw_reviews_stay_date_hsx")
-  #curl = driver.current_url
+  #Description scraping
   a = driver.find_element_by_class_name("cPQsENeY")
-  #for multiple elements(chnage element to elements) print(len(a))
-  #for i in a(a.text to i.text):
   rev = {'Description': a.text,
           #'Date of visit':DOV.text,
           #'Url extracted from':curl,
 
 
            }
-  
+  #Inserting into mongodb cloud  
   db.hotel_description.insert_one(rev)
 
   #closing tab and switching to main  
@@ -72,7 +64,7 @@ for i in range(len(res_buttons)):
   driver.switch_to.window(main_window)
   time.sleep(2)
 
-#print(driver.title)
+
 print('finished updating')
 
 
